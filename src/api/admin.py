dd24f9ea7ae5560d3bc421e68b5b9c578183a1dc
Alba Diaz <alba.dilop@gmail.com>
2025-12-05 18:08:49 +0000

    Clean up admin.py by removing unnecessary whitespace and comments

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,4 +1,3 @@
-  
 import os
 from flask_admin import Admin
 from .models import db, User, Profile, Review,Game,Match,Reject,Like
@@ -18,11 +17,6 @@ def setup_admin(app):
     admin.add_view(ModelView(Match, db.session))
     admin.add_view(ModelView(Reject, db.session))
     admin.add_view(ModelView(Like, db.session))
-
-
-
-
-
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
