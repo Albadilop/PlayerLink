dbfeeb78a5478dafbd1d1906d3ee206c2435540f
Alba Diaz <alba.dilop@gmail.com>
2025-12-09 15:26:04 +0000

    style: refine ProfileInfoTab CSS and tooltip functionality - Adjusted overflow properties, z-index values, and tooltip styles for improved visibility and layout. Enhanced tooltip content presentation for better user experience.

--- src/front/components/Profile/ProfileInfoTab.tsx
+++ src/front/components/Profile/ProfileInfoTab.tsx
@@ -391,23 +391,17 @@ export const ProfileInfoTab: React.FC<ProfileInfoTabProps> = ({
           {(["discord", "steam_id"] as const).map((f, i) => (
             <div key={i} className="col-md-6">
               <div className="info-field-card">
-                <label className="info-field-label">
+                <label className="info-field-label ">
                   <i
                     className={f === "steam_id" ? "fa-brands fa-steam" : "fa-brands fa-discord"}
                   ></i>
                   {f === "steam_id" ? "Steam Friend ID" : "Discord"}
-                  <span className="tooltip-wrapper">
-                    <i className="fa-solid fa-circle-info discord-info-icon"></i>
-                    <span className="tooltip-text discord-info-tooltip-text">
+                  <span className="tooltip-wrapper ">
+                    <i className="fa-solid fa-circle-info discord-info-icon "></i>
+                    <span className="tooltip-text">
                       <strong>Connect with your matches</strong>
-                      <div>
-                        The Discord or Steam info
-                        <br />
-                        in your profile will be <br />
-                        used by your matches
-                        <br />
-                        to reach out to you.
-                      </div>
+                      The Discord or Steam info in your profile will be used by your matches to
+                      reach out to you.
                     </span>
                   </span>
                 </label>
